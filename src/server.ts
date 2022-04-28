17158e87bf2cb2cba2138e3167b838d7f1a9bf45
AntLer (Arch) <antler@inbox.ru>
2022-04-28 11:06:20 +0700

    feat: добавлена поддержка TypeScript

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-const express = require('express')
-const { join } = require('path')
+import express, { Response } from 'express'
+import { join } from 'path'
 
 const publicPath = join(__dirname, '..', 'dist')
 
 const app = express()
 
 app.use(express.static(publicPath, { index: false }))
-app.use('*', (_, res) => res.sendFile(join(publicPath, 'index.html')))
+app.use('*', (_, res: Response) => res.sendFile(join(publicPath, 'index.html')))
 
 app.listen(3000, () => {
   console.log('Server listening port 3000...')
