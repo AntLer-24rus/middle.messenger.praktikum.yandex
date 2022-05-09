a1beee2f160cbe0ce6ef30703c73c2fc56b8c33f
AntLer (Arch) <antler@inbox.ru>
2022-05-09 22:53:46 +0700

    feat: переписана errorPage на компоненты

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import DevPanel from './modules/dev-panel'
 
 import LoginPage from './pages/login'
 import RegistrationPage from './pages/registration'
+import ErrorPage from './pages/error'
+import { Component } from './utils'
 
 document.addEventListener('DOMContentLoaded', () => {
   // if (process.env.NODE_ENV !== 'production') {
@@ -23,19 +25,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const pages = [
     { href: '/login', text: 'login', Comp: LoginPage },
     { href: '/registration', text: 'registration', Comp: RegistrationPage },
+    { href: '/error', text: 'error', Comp: ErrorPage },
+    { href: '/unknown', text: 'unknown', Comp: ErrorPage },
   ]
-  let page
+  let page: Component
   function renderPage(href: string) {
     window.history.pushState(null, '', href)
     const baseTitle = document.title.replace(/\s\|.*/gm, '')
     const Page = pages.find((p) => p.href === href)
     if (href === '/') {
       document.title = `${baseTitle} | home`
+      page = new LoginPage({})
     } else if (Page) {
       document.title = `${baseTitle} | ${Page.text}`
-      page = new Page.Comp({})
-      page.mount('#app')
+      let props = {}
+      if (href === '/error')
+        props = {
+          code: '500',
+          textError: 'Меняем электро проигрыватель на электро выигрыватель',
+        }
+      else if (href === '/unknown')
+        props = {
+          code: '404',
+          textError: 'Кажется такой страницы нет...',
+        }
+      page = new Page.Comp({ props })
+    } else {
+      page = new ErrorPage({
+        props: {
+          code: '404',
+          textError: 'Кажется такой страницы нет...',
+        },
+      })
     }
+    page.mount('#app')
   }
 
   const devPanel = new DevPanel({
--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,22 +1,23 @@
-import template from './error.hbs'
+import renderer from './error.hbs'
 import * as classes from './style.module.scss'
-import { button } from '../../modules'
+import { Button, Card } from '../../components'
+import { defineHBSComponent } from '../../utils'
 
 const buttonData = {
   text: 'Назад к чатам',
   type: 'stroke',
 }
 
-export const errorPage = {
-  name: 'error-page',
-  render: (data) => {
-    return template(
-      { ...data, button: buttonData, classes },
-      {
-        partials: {
-          button: button.render,
-        },
-      }
-    )
-  },
-}
+export default defineHBSComponent({
+  name: 'Error',
+  components: [Card, Button],
+  renderer,
+  props: { classes },
+  data: () => ({
+    buttonText: 'Назад к чатам',
+    buttonType: 'stroke',
+    btnClick(e: Event) {
+      console.log('Назад к чатам')
+    },
+  }),
+})
