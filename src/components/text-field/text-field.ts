537235e09aef138044785d73500a8093a352769a
AntLer (Arch) <antler@inbox.ru>
2022-05-11 13:38:38 +0700

    fix: исправлена ошибка потери фокуса в инпутах v2
    добавлен отдельный подкомпонент label для того чтобы при рендере не
    перерисовывался родительский компонент и input не терялся фокус

--- src/components/text-field/text-field.ts
+++ src/components/text-field/text-field.ts
@@ -1,9 +1,22 @@
 import renderer from './text-field.hbs'
 import classes from './text-field.module.scss'
-import { defineHBSComponent } from '../../utils'
+import { defineHBSComponent, last, Component } from '../../utils'
 import Input from './modules/input'
+import Label from './modules/label'
 
-export default defineHBSComponent({
+type TextFieldProps = {
+  validate: (val: string) => string
+  value: string
+  inputName: string
+  placeholder: string
+  error: string
+}
+type TextFieldData = {
+  classes: typeof classes
+  type: string
+}
+
+export default defineHBSComponent<TextFieldData, TextFieldProps>({
   name: 'TextField',
   renderer,
   props: {
@@ -13,19 +26,21 @@ export default defineHBSComponent({
     placeholder: 'введите текст',
     error: '',
   },
-  data() {
+  data(this: TextFieldProps) {
     return {
       classes,
-      //@ts-ignore
       type: this.inputName === 'password' ? 'password' : 'text',
-      validateInput(e: Event) {
+      validateInput(this: Component<TextFieldData & TextFieldProps>, e: Event) {
         const input = e.target as HTMLInputElement
         const error = this.data.validate(input.value)
-        this.parent.setProps({ error })
+        const errorLabel = this.parent!.children.find(
+          (c) => c.element === input.nextElementSibling
+        )
+        errorLabel!.setProps({ text: error })
       },
     }
   },
-  components: [Input],
+  components: [Input, Label],
   nativeEvents: {
     blur(e: Event) {},
   },
