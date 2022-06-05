c3f8dee5cd2ebbbd5817ba431b0d1ce60b51f77a
AntLer (Arch) <antler@inbox.ru>
2022-06-06 02:33:18 +0700

    feat: добавлен класс realtime сообщений

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { AuthService } from './auth.service'
 export { ChatsService } from './chats.service'
+export { RealTimeMessageService } from './realtimeMessage.service'
 export { ResourceService } from './resource.service'
 export { UsersService } from './users.service'
