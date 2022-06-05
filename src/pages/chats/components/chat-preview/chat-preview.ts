48129fa0744faaf446c7b266d4df8080c364c9aa
AntLer (Arch) <antler@inbox.ru>
2022-06-06 02:34:05 +0700

    refactor: добавлено отображение последнего сообщения
    раньше были заглушки

--- src/pages/chats/components/chat-preview/chat-preview.ts
+++ src/pages/chats/components/chat-preview/chat-preview.ts
@@ -13,6 +13,7 @@ type ChatPreviewData = {
   classes: typeof classes.default
   hasUnread: boolean
   avatarAlt: string
+  lastMessageTime: string | null
 }
 
 const props: ChatPreviewProps = {
@@ -25,9 +26,21 @@ const props: ChatPreviewProps = {
     unread_count: 0,
   },
 }
+
 const emits = {
   click: 'ChatPreview:click',
 }
+
+function formateDate(timestamp: string): string {
+  const date = new Date(timestamp)
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${day}.${month} ${hours}:${minutes}:${seconds}`
+}
+
 export default defineHBSComponent({
   name: 'ChatPreview',
   renderer,
@@ -42,6 +55,9 @@ export default defineHBSComponent({
         .map((word) => word[0].toUpperCase())
         .slice(0, 2)
         .join(''),
+      lastMessageTime: this.chatInfo.last_message
+        ? formateDate(this.chatInfo.last_message.time)
+        : null,
     }
   },
   DOMEvents: {
