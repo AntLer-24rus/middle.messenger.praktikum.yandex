b5692cb1f7a5722dc50b1112fae7af4806e37819
AntLer (Arch) <antler@inbox.ru>
2022-06-05 22:33:27 +0700

    refactor: исправлен реэкспорт страниц

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,13 +1,4 @@
-import { LoginPage } from './login'
-import { RegistrationPage } from './registration'
-import { ChatsPage } from './chats'
-import { ErrorPage } from './error'
-
+export { LoginPage } from './login'
+export { RegistrationPage } from './registration'
+export { ChatsPage } from './chats'
 export { ErrorPage } from './error'
-export const pages = [
-  { href: '/login', text: 'login', Comp: LoginPage },
-  { href: '/registration', text: 'registration', Comp: RegistrationPage },
-  { href: '/chats', text: 'chats', Comp: ChatsPage },
-  { href: '/error', text: 'error', Comp: ErrorPage },
-  { href: '/unknown', text: 'unknown', Comp: ErrorPage },
-]
